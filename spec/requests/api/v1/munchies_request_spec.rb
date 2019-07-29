34639128d02170533776e47b71bdd79c5ae05d50
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-29 10:13:22 -0600

    adds to munchie spec

--- spec/requests/api/v1/munchies_request_spec.rb
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-# RSpec.describe 'Munchies' do
-#   # VCR.use_cassette('requests/api/v1/background_response') do
-#     before(:each) do
-#       get '/api/v1/munchies'
-#     end
-#
-#     it "response is successful" do
-#       expect(response).to be_successful
-#     end
-#   # end
-# end
+RSpec.describe 'Munchies' do
+  # VCR.use_cassette('requests/api/v1/background_response') do
+    before(:each) do
+      get '/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese Content-Type: application/json Accept: application/jsons'
+    end
+
+    it "response is successful" do
+      expect(response).to be_successful
+    end
+
+end
