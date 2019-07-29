1a048bb6d0ab98c598202c7a70bb20007dc09657
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-29 06:36:54 -0600

    adds test for forecasts request

--- spec/requests/api/v1/forecasts_request_spec.rb
+++ spec/requests/api/v1/forecasts_request_spec.rb
@@ -1,12 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'Forecast' do
-  before(:each) do
-    get '/api/v1/forecast?location=denver,co'
-  end
+  VCR.use_cassette('requests/api/v1/forecast_response') do
+    before(:each) do
+      get '/api/v1/forecast?location=denver,co'
+    end
 
-  it "response is successful" do
-    expect(response).to be_successful
-  end
+    it "response is successful" do
+      expect(response).to be_successful
+    end
 
+    it "knows about city" do
+      full_response = JSON.parse(response.body, symbolize_names: true)
+
+      address = full_response[:google_results][:results].first[:formatted_address]
+
+      expect(address).to eq("Denver, CO, USA")
+    end
+
+    it "has current temperature" do
+      full_response = JSON.parse(response.body, symbolize_names: true)
+
+      current_temp = full_response[:forecast_data][:currently][:temperature]
+
+      expect(current_temp.class).to eq(Float)
+    end
+  end
 end
