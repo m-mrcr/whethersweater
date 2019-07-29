0ccfbe24d6d8d18a52c3ab12c4b3c5c88e3aad08
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-29 09:52:35 -0600

    adds bare bones spec for munchies controller

---
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+# RSpec.describe 'Munchies' do
+#   # VCR.use_cassette('requests/api/v1/background_response') do
+#     before(:each) do
+#       get '/api/v1/munchies'
+#     end
+#
+#     it "response is successful" do
+#       expect(response).to be_successful
+#     end
+#   # end
+# end
