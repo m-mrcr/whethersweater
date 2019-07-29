345dc8056f719e06dbab73b77f33ea6ce49d5706
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-29 09:52:41 -0600

    adds munchies route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
    namespace :v1 do
      get 'forecast', to: 'forecasts#show'
      get 'background', to: 'backgrounds#show'
+     get 'munchies', to: 'munchies#show'
    end
  end
 end
