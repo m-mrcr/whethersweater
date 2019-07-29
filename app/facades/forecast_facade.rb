5521ee427d269c48c1c840ada1587bb0490cb5c1
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-29 06:35:33 -0600

    adds forecast_facade

---
+++ app/facades/forecast_facade.rb
@@ -0,0 +1,35 @@
+class ForecastFacade
+  def initialize(location)
+    @location = location
+  end
+
+  def full_response
+    parameters = {
+      google_results: google_results,
+      forecast_data: forecast_data
+    }
+  end
+
+  private
+
+  def google_geocoding
+    params = { location: @location }
+    @google_geocoding ||= GoogleGeocoding.new(params)
+  end
+
+  def google_results
+    @google_results ||= google_geocoding.results
+  end
+
+  def get_coordinates
+    @coordinates ||= google_results[:results].first[:geometry][:location]
+  end
+
+  def dark_sky
+    @dark_sky ||= DarkSky.new(get_coordinates)
+  end
+
+  def forecast_data
+    @forecast_data ||= dark_sky.forecast
+  end
+end
