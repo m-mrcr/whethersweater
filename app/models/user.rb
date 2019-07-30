7a25f6f9143e28d98ae6fe65d93e2126b93be8d4
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-30 10:40:13 -0600

    adds user and add api key method

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
+
+  has_secure_password
+
+  before_save { self.email = self.email.to_s.downcase }
+  after_create :add_api_key
+
+  def add_api_key
+    self.update(api_key: SecureRandom.hex)
+  end
+end
