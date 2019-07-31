ab8ad2b1988918f9e802ce4329dae17b3b251ebb
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-31 10:44:09 -0600

    adds s to background(s) in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
    namespace :v1 do
      get 'forecast', to: 'forecasts#show'
-     get 'background', to: 'backgrounds#show'
+     get 'backgrounds', to: 'backgrounds#show'
      get 'munchies', to: 'munchies#show'
      post 'users', to: 'users#create'
      post 'sessions', to: 'sessions#create'
