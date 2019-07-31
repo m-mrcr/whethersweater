498de30a034bc8240471dd28cd7423b61d78eac7
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-31 08:46:25 -0600

    breaks out forecast methods into module

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,4 +1,5 @@
 class ForecastFacade
+  include ForecastMethods
   def initialize(location)
     @location = location
   end
@@ -8,28 +9,5 @@ class ForecastFacade
       location_data: geocoding,
       forecast_data: forecast_data
     }
-  end
-
-  private
-
-  def get_geocoding
-    params = { location: @location }
-    @get_geocoding ||= GoogleMaps.new(params)
-  end
-
-  def geocoding
-    @geocoding ||= get_geocoding.geocoding_response
-  end
-
-  def get_coordinates
-    @coordinates ||= geocoding[:results].first[:geometry][:location]
-  end
-
-  def dark_sky
-    @dark_sky ||= DarkSky.new(get_coordinates)
-  end
-
-  def forecast_data
-    @forecast_data ||= dark_sky.forecast
   end
 end
