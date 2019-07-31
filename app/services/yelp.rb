57696e0576cf37fb68286f0b09d43367692467b7
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-30 21:12:47 -0600

    removes get_json method from yelp service

--- app/services/yelp.rb
+++ app/services/yelp.rb
@@ -1,8 +1,19 @@
-class Yelp
+class Yelp < Base
+  def initialize(attributes)
+    @location = attributes[:location]
+    @food_term = attributes[:food]
+    @arrival_time = attributes[:arrival_time]
+  end
 
-  def get_json(uri, params = {})
-    response = conn.get uri, params
-    JSON.parse response.body, symbolize_names: true
+  def get_restaurants
+    params = {
+      location: @location,
+      open_at: @arrival_time,
+      categories: 'food',
+      term: @food_term,
+      limit: 3,
+    }
+    get_json('/v3/businesses/search', params)[:businesses]
   end
 
   private
