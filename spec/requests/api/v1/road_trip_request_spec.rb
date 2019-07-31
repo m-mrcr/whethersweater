c5acb9da9c19af1086dc94b8d0d340ca90454ca2
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-30 18:47:26 -0600

    adds headers for future implementation

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 RSpec.describe 'Road Trip' do
   before(:each) do
-    params = { "origin": "Denver,CO",
+    user = create(:user)
+    @api_key = user.api_key
+    @params = {
+               "origin": "Denver,CO",
                "destination": "Pueblo,CO",
-               "api_key": "jgn983hy48thw9begh98h4539h4"
+               "api_key": @api_key
              }
+    @headers = {
+                 'Content-Type': 'application/json',
+                 'Accept': 'application/json'
+               }
     # VCR.use_cassette('requests/api/v1/road_trip') do
-      post '/api/v1/road_trip', params: params
+      post '/api/v1/road_trip', params: @params
       # @full_response = JSON.parse(response.body, symbolize_names: true)
     # end
   end
