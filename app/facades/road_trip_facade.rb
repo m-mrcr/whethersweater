c32afb1b3308de505b31b094df5620ac63abb3f5
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-31 08:46:02 -0600

    adjusts how roadtrip facade is implemented

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,6 +1,22 @@
 class RoadTripFacade
-  def initialize(user_trip_parameters)
-    @origin = user_trip_parameters[:origin]
-    @destination = user_trip_parameters[:destination]
+  include DirectionsMethods
+  include ForecastMethods
+
+  def initialize(attributes)
+    @origin = attributes[:origin]
+    @destination = attributes[:destination]
+    @location = attributes[:destination]
+  end
+
+  def destination_forecast
+    forecast = forecast_data(arrival_time)
+    summary = forecast[:currently][:summary]
+    temperature = forecast[:currently][:temperature]
+    {summary: summary, temperature: temperature}
+  end
+
+  def estimated_travel_time
+    time = Time.at(travel_time).utc.strftime("%H:%M:%S").split(':')
+    return "#{time[0]} hour(s), #{time[1]} minute(s)"
   end
 end
