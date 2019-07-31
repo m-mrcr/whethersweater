18f215959e9307860f28f189b3c95b47a50c91de
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-31 00:05:29 -0600

    refactors forecasts request spec

--- spec/requests/api/v1/forecasts_request_spec.rb
+++ spec/requests/api/v1/forecasts_request_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Forecast' do
     VCR.use_cassette('requests/api/v1/forecast_response') do
       get '/api/v1/forecast?location=denver,co'
     end
+    @full_response = JSON.parse(response.body, symbolize_names: true)
   end
 
   it "response is successful" do
@@ -12,17 +13,13 @@ RSpec.describe 'Forecast' do
   end
 
   it "knows about city" do
-    full_response = JSON.parse(response.body, symbolize_names: true)
-
-    address = full_response[:location_data][:results].first[:formatted_address]
+    address = @full_response[:location_data][:results].first[:formatted_address]
 
     expect(address).to eq("Denver, CO, USA")
   end
 
   it "has current temperature" do
-    full_response = JSON.parse(response.body, symbolize_names: true)
-
-    current_temp = full_response[:forecast_data][:currently][:temperature]
+    current_temp = @full_response[:forecast_data][:currently][:temperature]
 
     expect(current_temp.class).to eq(Float)
   end
